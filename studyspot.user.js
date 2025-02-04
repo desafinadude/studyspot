f8e431bd06bc569fda831a630c1c1334b9737c9c
Di M <meerkdir@gmail.com>
2025-02-04 15:59:28 +0200

    Create studyspot.user.js

---
+++ studyspot.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         StudySpot
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Skeleton For Spotify Study App
+// @author       Desafinadude
+// @match        *://open.spotify.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    alert("StudySpot Loaded");
+})();
